f67752ac9053b9e95f1b10c47c71dde98dc235be
Umar Balak <umarbalak35@gmail.com>
2024-02-22 10:43:32 +0530

    shortened the variable names in CSS 2nd Exp

--- Crypto/exp1.py
+++ Crypto/exp1.py
@@ -2,48 +2,48 @@
 
 import string 
 # A list containing all characters 
-all_letters= string.ascii_letters
-print(f"All letters: '{all_letters}'\n")
+letters= string.ascii_letters
+print(f"All letters: '{letters}'\n")
 
 class SubstitutionCipher:
-    def encrypt(self, plainText, key = 4):
+    def encrypt(self, pTxt, key = 4):
         self.dict = {}
-        for i in range(len(all_letters)): 
-            self.dict[all_letters[i]] = all_letters[(i+key) % len(all_letters)] 
+        for i in range(len(letters)): 
+            self.dict[letters[i]] = letters[(i+key) % len(letters)] 
         cipher_txt_list = [] 
-        for char in plainText: 
-            if char in all_letters: 
+        for char in pTxt: 
+            if char in letters: 
                 temp = self.dict[char] 
                 cipher_txt_list.append(temp) 
             else: 
                 temp = char 
                 cipher_txt_list.append(temp)
-        cipherText = "".join(cipher_txt_list) 
-        return cipherText, key
+        cTxt = "".join(cipher_txt_list) 
+        return cTxt, key
 
-    def decrypt(self, cipherText, key = 4):
+    def decrypt(self, cTxt, key = 4):
         self.dict = {}
-        for i in range(len(all_letters)): 
-            self.dict[all_letters[i]] = all_letters[(i-key) % len(all_letters)] 
-        plain_txt_list = [] 
-        for char in cipherText: 
-            if char in all_letters: 
+        for i in range(len(letters)): 
+            self.dict[letters[i]] = letters[(i-key) % len(letters)] 
+        pTxtList = [] 
+        for char in cTxt: 
+            if char in letters: 
                 temp = self.dict[char] 
-                plain_txt_list.append(temp) 
+                pTxtList.append(temp) 
             else: 
                 temp = char 
-                plain_txt_list.append(temp)
-        plainText = "".join(plain_txt_list) 
-        return plainText, key
+                pTxtList.append(temp)
+        pTxt = "".join(pTxtList) 
+        return pTxt, key
     
 
-plainText = "Cryptography And System Security"
-print(f"Original Text is: '{plainText}'\n")
+pTxt = "Cryptography And System Security"
+print(f"Original Text is: '{pTxt}'\n")
 
 subCipher = SubstitutionCipher()
-cipherText, key = subCipher.encrypt(plainText)
-print(f"Cipher/Encrypted Text is: '{cipherText}'\nKey for encryption is: '{key}'\n")
+cTxt, key = subCipher.encrypt(pTxt)
+print(f"Cipher/Encrypted Text is: '{cTxt}'\nKey for encryption is: '{key}'\n")
 
-plainText, key = subCipher.decrypt(cipherText)
-print(f"Plain/Decrypted Text is: '{plainText}'\nKey for decryption is: '{key}'")
+pTxt, key = subCipher.decrypt(cTxt)
+print(f"Plain/Decrypted Text is: '{pTxt}'\nKey for decryption is: '{key}'")
 
